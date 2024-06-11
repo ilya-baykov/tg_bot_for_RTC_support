795b6163b83a4741ebd19357531d5294511e8b78
ilya-baykov <i-baykov@internet.ru>
2024-06-11 22:42:04 +0300

    Теперь полученные задачи сортируются по времени

--- bot/database/methods/get.py
+++ bot/database/methods/get.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import select
+from sqlalchemy import select, asc
 
 from bot.database.database_main import session_factory
 from bot.database.models.main_models import TaskEntry
@@ -8,7 +8,11 @@ from bot.database.models.main_models import TaskEntry
 
 def select_future_tasks():
     with session_factory() as session:
-        query = select(TaskEntry).filter(TaskEntry.scheduled_time > datetime.datetime.now())
+        query = (
+            select(TaskEntry)
+            .filter(TaskEntry.scheduled_time > datetime.datetime.now())
+            .order_by(asc(TaskEntry.scheduled_time))
+        )
         result = session.execute(query)
         future_tasks = result.scalars().all()
         print(future_tasks)
