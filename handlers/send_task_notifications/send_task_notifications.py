9364b33ead546cb447d2d2b40e43f58ce4775157
ilya-baykov <i-baykov@internet.ru>
2024-06-13 23:19:10 +0300

    Добавил класс TaskMananager

--- handlers/send_task_notifications/send_task_notifications.py
+++ handlers/send_task_notifications/send_task_notifications.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from aiogram import Dispatcher, Bot, Router, F
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message, CallbackQuery
@@ -17,27 +19,27 @@ send_task_router = Router()
 db = Database()
 
 
-async def send_task_notifications(bot: Bot, tasks):
-    try:
-        for task in tasks:
-            employee = await db.get_employee_by_telegram_username(task.employee_telegram)
-            if employee:
-                process = await db.get_process_by_name(task.process_name)
-                if process:
-                    message_text = (
-                        f"Привет, {employee.name}!\n"
-                        f"Ваша задача: {process.process_name}\n"
-                        f"Описание: {process.action_description}\n"
-                        f"Запланировано на: {task.scheduled_time}"
-                    )
-                    try:
-                        await bot.send_message(chat_id=employee.telegram_id, text=message_text, reply_markup=keyboard)
-
-                        logger.info(f"Отправлено уведомление о задаче для сотрудника {employee.name}")
-                    except Exception as e:
-                        logger.error(f"Ошибка при отправке уведомления для сотрудника {employee.name}: {e}")
-    except Exception as e:
-        logger.error(f"Ошибка при получении задач из базы данных: {e}")
+# async def send_task_notifications(bot: Bot, tasks):
+# try:
+#     for task in tasks:
+#         employee = await db.get_employee_by_telegram_username(task.employee_telegram)
+#         if employee:
+#             process = await db.get_process_by_name(task.process_name)
+#             if process:
+#                 message_text = (
+#                     f"Привет, {employee.name}!\n"
+#                     f"Ваша задача: {process.process_name}\n"
+#                     f"Описание: {process.action_description}\n"
+#                     f"Запланировано на: {task.scheduled_time}"
+#                 )
+#                 try:
+#                     await bot.send_message(chat_id=employee.telegram_id, text=message_text, reply_markup=keyboard)
+#
+#                     logger.info(f"Отправлено уведомление о задаче для сотрудника {employee.name}")
+#                 except Exception as e:
+#                     logger.error(f"Ошибка при отправке уведомления для сотрудника {employee.name}: {e}")
+# except Exception as e:
+#     logger.error(f"Ошибка при получении задач из базы данных: {e}")
 
 
 # @send_task_router.message(Command(WaitUserResponse.task_send))
@@ -45,13 +47,22 @@ async def send_task_notifications(bot: Bot, tasks):
 #     await state.set_state(WaitUserResponse.response)
 
 
-async def start_send_task_notifications(bot: Bot, dp: Dispatcher, tasks):
-    while True:
-        await send_task_notifications(bot, tasks)
-        await asyncio.sleep(3600)
+# async def start_send_task_notifications(bot: Bot, dp: Dispatcher, tasks):
+#     while True:
+#         await send_task_notifications(bot, tasks)
+#         await asyncio.sleep(3600)
+#
+#
+# def register_send_task_handlers(dp: Dispatcher, bot: Bot, tasks):
+#     asyncio.create_task(start_send_task_notifications(bot, dp, tasks))
+#
+# # Работа с FSMContext. !!!!
+
+@send_task_router.message(WaitUserResponse.task_send)
+async def send_message(message: Message, state: FSMContext):
+    pass
 
 
-def register_send_task_handlers(dp: Dispatcher, bot: Bot, tasks):
-    asyncio.create_task(start_send_task_notifications(bot, dp, tasks))
-
-# Работа с FSMContext. !!!!
+@send_task_router.message(WaitUserResponse.response)
+async def get_user_response(message: Message, state: FSMContext, tasks: List):
+    pass
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from aiogram.client.bot import DefaultBotProperties
 from aiogram.enums.parse_mode import ParseMode
 
 from database.Database import Database
+from handlers.send_task_notifications.TaskManager import TaskManager
 from handlers.start.start import register_start_handlers
-from handlers.send_task_notifications.send_task_notifications import register_send_task_handlers
+# from handlers.send_task_notifications.send_task_notifications import register_send_task_handlers
 from handlers.user_answer.user_answer import register_user_response
 
 # Установите политику цикла событий для Windows
@@ -23,13 +24,15 @@ async def start():
     try:
         db = Database()
         # await db.create_db()
-        tasks = await db.select_future_tasks()  # Получаем задачи один раз при запуске
+        # tasks = await db.select_future_tasks()  # Получаем задачи один раз при запуске
 
         # Регистрация обработчиков
         register_start_handlers(dp)
-        register_send_task_handlers(dp, bot, tasks)
-        register_user_response(dp)
+        # register_send_task_handlers(dp, bot, tasks)
+        # register_user_response(dp)
 
+        task_manager = TaskManager(bot, db)
+        await task_manager.load_tasks()
         await dp.start_polling(bot, skip_updates=True)
     finally:
         await bot.session.close()
@@ -37,3 +40,4 @@ async def start():
 
 if __name__ == '__main__':
     asyncio.run(start())
+# 2024-06-14 02:10:10
