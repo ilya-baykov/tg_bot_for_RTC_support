7df48382f7a58e258dc6d5cf9383360a98e34cbd
ilya-baykov <i-baykov@internet.ru>
2024-06-17 16:44:06 +0300

    Рефакторинг main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from os import environ
 
-from aiogram import Dispatcher, Bot, F
+from aiogram import Dispatcher, Bot
 import asyncio
 import platform
 
-from database.Database import Database
-from handlers.send_task_notifications.TaskManager import TaskManager
+from database.Database import DataBase, InputDB, EmployeesDB, ProcessDB, NotificationDB
 from handlers.start.start import register_start_handlers
 from handlers.send_task_notifications.send_task_notifications import add_jobs
-# from handlers.send_task_notifications.send_task_notifications import register_send_task_handlers
-from handlers.user_answer.user_answer import register_user_response
 
 # Установите политику цикла событий для Windows
 if platform.system() == 'Windows':
@@ -17,26 +14,21 @@ if platform.system() == 'Windows':
 
 bot = Bot(token=environ.get('TOKEN', 'define me!'))
 dp = Dispatcher()
+db = DataBase()
+inputdb, processesdb = InputDB(db), ProcessDB(db)
 
 
 async def start():
     try:
-        db = Database()
-        tasks = await db.select_future_tasks()
-        await db.create_processes(tasks)
-        processes = await db.get_all_processes()
+        # await db.create_db()
+        tasks = await inputdb.get_tasks()
+        processes = await processesdb.create_new_processes(tasks, db)
 
         await add_jobs(bot, processes)
-        # await db.create_db()
-        # tasks = await db.select_future_tasks()  # Получаем задачи один раз при запуске
 
         # Регистрация обработчиков
         register_start_handlers(dp)
-        # register_send_task_handlers(dp, bot, tasks)
-        # register_user_response(dp)
 
-        task_manager = TaskManager(bot, db)
-        await task_manager.load_tasks()
         await dp.start_polling(bot, skip_updates=True)
     finally:
         await bot.session.close()
