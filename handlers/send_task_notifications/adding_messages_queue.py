6d72149d6f2d88e560b5366a26829df66bf6c616
ilyab <i-baykov@internet.ru>
2024-06-19 17:50:40 +0300

    Заменил смену статусов в функции добавлении задач в очередь

--- handlers/send_task_notifications/adding_messages_queue.py
+++ handlers/send_task_notifications/adding_messages_queue.py
@@ -3,11 +3,11 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from handlers.send_task_notifications.keyboard import keyboard
 import logging
-from database.Database import DataBase, ProcessDB, ProcessStatus, EmployeeStatus
+from database.Database import DataBase, ProcessDB, EmployeesDB, ProcessStatus, EmployeeStatus
 
 db = DataBase()
 processDB = ProcessDB(db)
-
+employeesDB = EmployeesDB(db)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -33,24 +33,10 @@ async def sent_message(bot: Bot, process):
     text = f"Имя процесса: {process.process_name}\nОписание процесса:{process.action_description}"
     print(text)
     employee = process.employee
+    await processDB.change_status(process, status=ProcessStatus.sent)
+    await employeesDB.change_status(employee=employee, status=EmployeeStatus.busy)
 
-    async with db.Session() as session:
-        async with session.begin():
-            # Получаем текущие объекты из сессии для отслеживания изменений
-            employee = await session.merge(employee)
-            process = await session.merge(process)
-
-            # Вносим изменения
-            employee.status = EmployeeStatus.busy
-            process.status = ProcessStatus.sent
-
-            # Копируем необходимые данные перед коммитом
-            telegram_id = employee.telegram_id
-
-            # Сохраняем изменения
-            await session.commit()
-
-    await bot.send_message(chat_id=telegram_id, text=text, reply_markup=keyboard)
+    await bot.send_message(chat_id=employee.telegram_id, text=text, reply_markup=keyboard)
 
 
 async def add_jobs(bot, process):
