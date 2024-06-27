6ddd757e40a9c2e3a5c04458e6ab2e24eeb4a877
ilya-baykov <i-baykov@internet.ru>
2024-06-27 17:06:51 +0300

    Реализовал алгоритм паузы и возобновления задачи из планировщика , в случаи редактирования предыдущих задач сотрудником

--- handlers/edit/edit_report_task.py
+++ handlers/edit/edit_report_task.py
@@ -1,16 +1,20 @@
+from datetime import datetime, timedelta
 import logging
 
 from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, CallbackQuery
+from apscheduler.jobstores.base import JobLookupError
+from apscheduler.triggers.date import DateTrigger
 
-from database.CRUD.read import ActionsTodayReader, EmployeesReader, ReportReader
-from database.CRUD.update import ReportUpdater
-from database.enums import FinalStatus
+from database.CRUD.read import ActionsTodayReader, EmployeesReader, ReportReader, SchedulerTasksReader
+from database.CRUD.update import ReportUpdater, SchedulerTasksUpdater
+from database.enums import FinalStatus, SchedulerStatus
 from handlers.edit.keyboard import inline_today, TaskInfo
 from handlers.filters_general import RegisteredUser
+from run_app.main_objects import scheduler
 from sent_task_to_emploeyee.keyboard import keyboard
 
 logging.basicConfig(level=logging.INFO)
@@ -37,11 +41,26 @@ async def edit_task(message: Message, state: FSMContext):
 
 @edit_router.callback_query(TaskInfo.filter())
 async def process_task_selection(callback_query: CallbackQuery, callback_data: TaskInfo, state: FSMContext):
+    employee = await EmployeesReader.get_employee_by_telegram_id_or_username(
+        telegram_id=str(callback_query.from_user.id))  # Получаем сотрудника
+
+    # ДеЙствие планировщика поставить на паузу
+    task_in_scheduler = await  SchedulerTasksReader().get_last_task_by_employee(employee_id=employee.id)
+
+    # Остановка задачи из планировщика по ID
+    try:
+        scheduler.pause_job(task_in_scheduler.id)
+        # Изменяем статус задачи в планировщике
+        await SchedulerTasksUpdater.update_params(task=task_in_scheduler, status=SchedulerStatus.suspended)
+        logger.info(f"Задача с ID {task_in_scheduler.id} остановлена.")
+    except JobLookupError:
+        logger.error(f"Задача с ID {task_in_scheduler.id} не найдена в планировщике.")
+
     await callback_query.answer()
     await state.set_state(EditState.on_edit)
 
     # Сохраняем номер задач для редактирования
-    await state.update_data(task_id=callback_data.task_id)
+    await state.update_data(task_id=callback_data.task_id, scheduler_task_id=task_in_scheduler.id)
 
     await callback_query.message.answer(
         text=f"Отредактируйте задачу {callback_data.task_id}",
@@ -61,13 +80,34 @@ async def test(message: Message, state: FSMContext):
 
 @edit_router.message(EditState.write_comment)
 async def test_2(message: Message, state: FSMContext):
+    current_time = datetime.now()
     user_data = await state.get_data()
     task_id = user_data.get("task_id")
+    task_in_scheduler_id = user_data.get('scheduler_task_id')
+    print(task_in_scheduler_id)
+    task_in_scheduler = await SchedulerTasksReader.get_tasks(task_in_scheduler_id)
+
+    # Если следующая задача должна была выполниться - переводим время
+    if task_in_scheduler.expected_completion_time > current_time + timedelta(seconds=5):
+        time = task_in_scheduler.expected_completion_time
+    else:
+        time = task_in_scheduler.expected_completion_time + timedelta(seconds=5)
+
+    await SchedulerTasksUpdater.update_params(task=task_in_scheduler,
+                                              status=SchedulerStatus.awaiting_dispatch,
+                                              time=time)
+    new_run_time = time
+    new_trigger = DateTrigger(run_date=new_run_time)
+
+    scheduler.modify_job(task_in_scheduler_id, trigger=new_trigger)
+    logger.info(f"Время выполнения задачи с ID {task_in_scheduler_id} установлено на :{new_run_time}.")
+
     status = user_data.get("status")
     report = await ReportReader().get_report_by_actions_id(int(task_id))
     await ReportUpdater().update_params(report, status=status, comment=message.text)
     await state.clear()
     await message.answer("Изменеия успешно сохранились в таблицу.")
+    # Возобновить действие из планировщика
 
 
 def register_edit_handlers(dp):
