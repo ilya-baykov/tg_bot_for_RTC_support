522b180cb59c361511e05feabb12c2e50e3e3a42
ilyab <i-baykov@internet.ru>
2024-07-30 10:46:12 +0300

    Добавл настройки логгирования

---
+++ logger_settings/setup_logger.py
@@ -0,0 +1,16 @@
+from logging import getLogger, basicConfig, DEBUG, FileHandler
+from datetime import datetime
+import os
+
+LOG_FORMAT = '%(asctime)s : %(name)s : %(levelname)s : %(message)s'
+LOGS_DIR = os.getenv("LOGS_PATH", "define me!")  # получаем путь к папке LOGS
+os.makedirs(LOGS_DIR, exist_ok=True)  # Проверка наличия папки ( при ее отсуствии - создание )
+
+
+def setup_logger():
+    logger = getLogger()
+    log_file_path = os.path.join(LOGS_DIR, f"Logger_{datetime.today().strftime('%d_%m_%Y')}.log")
+    file_handler = FileHandler(log_file_path, mode="a")
+    file_handler.setLevel(DEBUG)
+    basicConfig(level=DEBUG, format=LOG_FORMAT, handlers=[file_handler])
+    return logger
