74d8a3a164e217c95634596f549ffc394ac91de5
ilyab <i-baykov@internet.ru>
2024-08-03 17:49:56 +0300

    Добавил обработку отложенных задач

--- handlers/postponed/deferred_processes.py
+++ handlers/postponed/deferred_processes.py
@@ -5,12 +5,14 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 
-from database.CRUD.read import EmployeesReader, SchedulerTasksReader, ActionsTodayReader
+from database.CRUD.read import EmployeesReader, SchedulerTasksReader, ActionsTodayReader, ReportReader
+from database.CRUD.update import ReportUpdater
+from database.enums import FinalStatus
 from handlers.filters_general import RegisteredUser
 from handlers.postponed.keyboard import inline_deferred_tasks, ActionInfo, keyboard_for_report
 from handlers.postponed.state import PostponedState
 from run_app.main_objects import scheduler
-from utility.sheduler_functions import pause_scheduler_task
+from utility.sheduler_functions import pause_scheduler_task, resume_scheduler_task
 
 logger = logging.getLogger(__name__)
 
@@ -46,29 +48,29 @@ async def take_selection_deferred_tasks(callback_query: CallbackQuery, callback_
         reply_markup=keyboard_for_report)
 
 
-# @edit_router.message(EditState.on_edit, F.text.in_({"Выполнено", "Не выполнено"}))
-# async def state_update_data(message: Message, state: FSMContext):
-#     await state.set_state(EditState.write_comment)
-#     if message.text == "Выполнено":
-#         await state.update_data({"status": FinalStatus.successfully})
-#     elif message.text == "Не выполнено":
-#         await state.update_data({"status": FinalStatus.failed})
-#     await message.answer("Напиши комментарий")
-#
-#
-# @edit_router.message(EditState.write_comment)
-# async def change_entry_report(message: Message, state: FSMContext):
-#     user_data = await state.get_data()
-#     task_id = user_data.get("task_id")
-#     task_in_scheduler_id = user_data.get('scheduler_task_id')
-#     if task_in_scheduler_id:
-#         await resume_scheduler_task(task_in_scheduler_id)
-#     status = user_data.get("status")
-#     report = await ReportReader().get_report_by_id(int(task_id))
-#     await ReportUpdater().update_params(report, status=status, comment=message.text)
-#     await state.clear()
-#     await message.answer("Изменеия успешно сохранились в таблицу.")
-#
-#
+@postponed_router.message(PostponedState.getting_task, F.text.in_({"Выполнено", "Не выполнено"}))
+async def state_update_data(message: Message, state: FSMContext):
+    await state.set_state(PostponedState.task_processing)
+    if message.text == "Выполнено":
+        await state.update_data({"status": FinalStatus.successfully})
+    elif message.text == "Не выполнено":
+        await state.update_data({"status": FinalStatus.failed})
+    await message.answer("Напиши комментарий")
+
+
+@postponed_router.message(PostponedState.task_processing)
+async def change_entry_report(message: Message, state: FSMContext):
+    user_data = await state.get_data()
+    task_id = user_data.get("task_id")
+    task_in_scheduler_id = user_data.get('scheduler_task_id')
+    if task_in_scheduler_id:
+        await resume_scheduler_task(task_in_scheduler_id)
+    status = user_data.get("status")
+    report = await ReportReader().get_report_by_id(int(task_id))
+    await ReportUpdater().update_params(report, status=status, comment=message.text)
+    await state.clear()
+    await message.answer("Изменеия успешно сохранились в таблицу.")
+
+
 def register_postponed_handlers(dp):
     dp.include_router(postponed_router)
